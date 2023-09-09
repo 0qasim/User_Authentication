e46abe5a6c4e625299ad738d4575b95d8dc49c44
Muhammad Qasim <144331636+0qasim@users.noreply.github.com>
2023-09-09 16:20:08 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   const [user,setUser]=useState({})
   axios.defaults.withCredentials=true;
   useEffect(() =>{
-    axios.get('https://user-authentication-mu.vercel.app')
+    axios.get('https://user-authentication-mu.vercel.app/')
     .then(user =>{
 
       console.log(user)
